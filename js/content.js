e60de66811ae7d7fdbd68f493ca0b2478678f185
aayusha2042 <qsb23@txstate.edu>
2026-09-18 19:49:38 +0000

    minor adjustments

--- js/content.js
+++ js/content.js
@@ -100,7 +100,7 @@ const AppCopy = (() => {
     estimatedPerService: "Estimated value per service",
     estimatedTotal: "Estimated total value",
     addLabel: "Add another service",
-    emptyState: "No services reported for this period",
+    emptyState: "No services to report for this period",
     next: "See impact summary",
     back: "Back",
   };
